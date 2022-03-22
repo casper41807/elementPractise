04f6a6c00a2bd1bfe20e892596810632b83983bf
casper41807 <casper41807@gmail.com>
2022-03-22 15:24:35 +0800

    Initial Commit

---
+++ element elementPractise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  element elementPractise
+//
+//  Created by 陳秉軒 on 2022/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
